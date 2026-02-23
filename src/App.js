a73a4ef8dbeee478132470e773d6f588fe6bb408
zahidul96 <zahidulhussain111@gmail.com>
2026-02-23 16:02:17 +0600

    Added summary of the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "./components/navbar/Header";
+import MealsSummary from "./Meals/MealsSummary";
 const App=()=> {
   return (
     <div>
       <Header/>
+      <MealsSummary/>
     </div>
   );
 }
